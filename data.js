63453b938d6c6717e259869fa11a870acb6dd0a7
Marcos Ribeiro <Marcos@Anas-Mac-mini.local>
2021-07-21 10:22:27 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,64 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1111",
+      "name": "1111",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.8305093983371972,
+        "pitch": 0.19514985984110922,
+        "fov": 1.4513841934209655
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9682863361405367,
+          "pitch": 0.0083087552565555,
+          "rotation": 0,
+          "target": "1-22222"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-22222",
+      "name": "22222",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 79.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
